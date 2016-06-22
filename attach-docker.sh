49cf392efe8595247f06f6abd56dbf0f20582954
ziggy <ziggy@elephant-bird.net>
2016-06-22 12:13:50 -0400

    Revise start docker, add attach script

---
+++ attach-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# useful: http://askubuntu.com/questions/505506/how-to-get-bash-or-ssh-into-a-running-container-in-background-mode
+docker exec -i -t ls1 /bin/bash
+
+
--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd littlesis-apps/
-docker run -it -p 9000:80 -p 9001:81 -v `pwd`:/var/www/littlesis littlesis:v0.0.3
+docker run -d --name ls1 -p 9000:80 -p 9001:81 -v `pwd`:/var/www/littlesis littlesis:v0.0.3 tail -f /dev/null
