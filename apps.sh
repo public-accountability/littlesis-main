b2ebc7fca1e4ef1ace1a90edda360df7fe79f041
ziggy <ziggy@elephant-bird.net>
2016-11-14 20:56:55 -0500

    Copy configurations

--- apps.sh
+++ apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p littlesis-apps
-cd littlesis-apps
+mkdir -p apps
+cd apps
 mkdir symfony && cd symfony && git clone git@github.com:littlesis-org/littlesis.git . && ./setup.sh
 cd ..
 mkdir rails && cd rails && git clone git@github.com:skomputer/lilsis.git . && ./setup.sh
