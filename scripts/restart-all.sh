43d2e3e31de7579e92e9b9f450f4bf342d6a9716
ziggy <ziggy@elephant-bird.net>
2017-02-21 18:24:08 -0500

    bye bye even more useless scripts

--- scripts/restart-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /var/www/littlesis/rails && bin/delayed_job restart
-cd /var/www/littlesis/rails && bundle exec rake ts:restart
-service apache2 restart
-# service mysql restart
-service memcached restart
-searchd --config /var/www/littlesis/symfony/config/sphinx.conf
-
