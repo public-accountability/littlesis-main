db16fd53ad40f06b3202cb8de4bd5be789af1890
ziggy <ziggy@elephant-bird.net>
2017-02-28 09:58:32 -0500

    Update rails dockerfile

--- apps.sh
+++ apps.sh
@@ -4,5 +4,5 @@ mkdir -p apps
 cd apps
 mkdir symfony && cd symfony && git clone git@github.com:littlesis-org/littlesis.git . && ./setup.sh
 cd ..
-mkdir rails && cd rails && git clone git@github.com:skomputer/lilsis.git . && ./setup.sh
+mkdir rails && cd rails && git clone git@github.com:public-accountability/littlesis-rails.git . && ./setup.sh
 
