94a59d5671314b6167451f0c84d77d00ddfebb1a
ziggy <ziggy@elephant-bird.net>
2017-04-11 15:37:13 -0400

    rebuild image - upgraded gems

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --no-cache -t ls-rails:v0.0.8 -f passenger.docker .
+docker build --no-cache -t ls-rails:v0.0.9 -f passenger.docker .
 docker build -t ls-php:v0.0.5 -f php.docker .
