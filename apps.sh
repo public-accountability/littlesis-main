83e946d4729801cb91c3a34f20a09494691e5321
ziggy <ziggy@elephant-bird.net>
2017-07-26 15:14:22 -0400

    remove config.sh and apps.sh + add dev configuration

--- apps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p apps
-cd apps
-mkdir symfony && cd symfony && git clone git@github.com:littlesis-org/littlesis.git . && ./setup.sh
-cd ..
-mkdir rails && cd rails && git clone git@github.com:public-accountability/littlesis-rails.git . && ./setup.sh
-
