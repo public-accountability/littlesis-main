7b724ac3757c7c9fc8fbf4e273e6920e478a0fcf
ziggy <ziggy@elephant-bird.net>
2018-07-30 17:10:23 -0400

    skip host checking when determine scheme to use in clone.sh

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,7 +2,7 @@
 
 mkdir -p rails
 
-if ssh -T git@github.com 2>&1 | grep -q 'successfully authenticated';then
+if ssh -o StrictHostKeyChecking=no -T git@github.com 2>&1 | grep -q 'successfully authenticated';then
     URL="git@github.com:public-accountability/littlesis-rails.git"
 else 
     URL="https://github.com/public-accountability/littlesis-rails.git"
