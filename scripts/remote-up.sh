0b04d4fd25ebb49220679b53eca8615aba0adb62
ziggy <ziggy@elephant-bird.net>
2018-07-31 09:42:07 -0400

    add remote scripts to littlesis program
    + remove unneeded while loop

--- scripts/remote-up.sh
+++ scripts/remote-up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sshfs littlesis-dev:/littlesis ~/littlesis
+if ! mountpoint -q ~/littlesis
+then
+    sshfs littlesis-dev:/littlesis ~/littlesis
+fi
 
-
+ssh -4 -T -L 8080:localhost:8080 -L 8888:localhost:8888 -L 3306:localhost:3306 littlesis-dev
