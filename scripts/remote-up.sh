2dbb5c9e1ee110a79f577a1d5dc7b34fc0ac8f5f
ziggy <ziggy@elephant-bird.net>
2018-08-01 12:18:15 -0400

    revise `littlesis remote up/down`. add `littlesis remote edit`

--- scripts/remote-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if ! mountpoint -q ~/littlesis
-then
-    sshfs littlesis-dev:/littlesis ~/littlesis
-fi
-
-ssh -4 -T -L 8080:localhost:8080 -L 8888:localhost:8888 -L 3306:localhost:3306 littlesis-dev
