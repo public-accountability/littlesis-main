8205967bf4536e7a1b67a822282005c5a35ca2e9
ziggy <ziggy@littlesis.org>
2019-02-13 10:34:51 -0500

    add oligrapher_littlesis_bridge file
    Includes LsDataSource and LsDataConverter

--- scripts/download_oligrapher_version.fish
+++ scripts/download_oligrapher_version.fish
@@ -7,12 +7,14 @@ function download_oli -a version_number
     set oligrapher_tar $tmp_dir/oligrapher.tar.gz
     set oligrapher_js_file "oligrapher-$version_number.js"
     set oligrapher_source_map_file "oligrapher-$version_number.js.map"
-    
+    set littlesis_bridge_file "oligrapher_littlesis_bridge-$version_number.js"
+
     curl -sSL $url > $oligrapher_tar
-    sh -c "cd $tmp_dir && tar -z -f $oligrapher_tar --strip-components=2 --wildcards -x '*build/oligrapher.js*'"
+    sh -c "cd $tmp_dir && tar -z -f $oligrapher_tar --strip-components=2 --wildcards -x '*build/oligrapher.js*' -x '*build/LsData*.js*'"
 
     cat $tmp_dir/oligrapher.js | sed "s/sourceMappingURL=oligrapher.js.map/sourceMappingURL=$oligrapher_source_map_file/" > $oligrapher_js_file
     cp $tmp_dir/oligrapher.js.map $oligrapher_source_map_file
+    cat $tmp_dir/LsDataConverter.js $tmp_dir/LsDataSource.js | sed 's/"use strict";//g' > $littlesis_bridge_file
     
     rm -rf $tmp_dir
 end
