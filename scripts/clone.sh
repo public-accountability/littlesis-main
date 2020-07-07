83a18174c93f6e39e849a23a41aac00e3b9f317e
ziggy <ziggy@littlesis.org>
2020-07-07 13:00:51 -0400

    don't run setup.sh after cloning

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 
 mkdir -p rails
 
 if ssh -o StrictHostKeyChecking=no -T git@github.com 2>&1 | grep -q 'successfully authenticated';then
     URL="git@github.com:public-accountability/littlesis-rails.git"
-else 
+else
     URL="https://github.com/public-accountability/littlesis-rails.git"
 fi
 
-cd rails && git clone $URL . && ./setup.sh
+cd rails && git clone $URL .
